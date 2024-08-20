929a1ed30a3e50a839800fc91ce09fe265113e9b
boykalanders <boykalanders@gmail.com>
2024-08-20 10:26:02 -0500

    feat: add parameter with rebalance period in main.py

--- lib_rebalance.py
+++ lib_rebalance.py
@@ -2,13 +2,15 @@ import numpy as np
 import main as m
 from math import sqrt
 import pandas as pd
+from datetime import datetime
 
-def get_lp_evaluation_scenarios():
+def get_lp_evaluation_scenarios(pool, wallet_created_at):
     
     
     date_begin = '2021-05-05' # price: 0.060538
     date_end = '2023-12-20' # price: 0.060388
 
+    last_day = '2024-02-28'
     list_scenarios = [
         ['recent 1 year backtest', '2022-12-15', '2023-12-15'],
         ['1 year price revert-back', '2022-05-13', '2023-05-13'],
@@ -16,13 +18,14 @@ def get_lp_evaluation_scenarios():
         ['1 year maximum price-up', '2022-06-12', '2023-06-12' ],
         ['history case max price-down','2021-12-09',  '2022-06-19' ],
         ['history case max price-up','2022-06-19',  '2022-09-09' ], 
-        ['history case max first-down-then-up','2021-12-09',  '2022-09-09' ]
+        ['history case max first-down-then-up','2021-12-09',  '2022-09-09' ],
+        # ['from the created date of the wallet', wallet_created_at, last_day]
         
     ]
 
     list_scenarios_name = ['scenario_name', 'date_begin', 'date_end']
 
-    df_price = m.get_df_daily_price(date_begin,date_end)
+    df_price = m.get_df_daily_price(pool, date_begin,date_end)
     
     df_scenarios = pd.DataFrame(list_scenarios, columns=list_scenarios_name)
     df_scenarios['index'] = df_scenarios.index
@@ -43,14 +46,11 @@ def get_lp_evaluation_scenarios():
     # reset index as 0,1,... to maintan the sequence
     df_scenarios.set_index("index", inplace=True)
     df_scenarios.sort_index(ascending=True, inplace=True)
-
+    
     return df_scenarios
 
 
-
-
 def get_lp_asset_qty_after_price_chg(p0, pn, qty0, qty1, range_down, range_up, b_input_pct = True):
-    
     # convert to price
     if b_input_pct: 
         range_down = p0*(1+range_down)
@@ -69,7 +69,6 @@ def get_lp_asset_qty_after_price_chg(p0, pn, qty0, qty1, range_down, range_up, b
         pn = range_up
     else:
         pn=pn
-
 
     p0_sqrt = sqrt(p0)
     pn_sqrt = sqrt(pn)
@@ -111,8 +110,6 @@ import lib_logic
 range_down = -0.1
 range_up = -1*range_down/(1+range_down) 
 
-
-
 def portfolio_value_no_rebalance(df, range_down, range_up, initial_qty_0_and_1 = None, benchmark_avg_yld_range = -0.3 ):
     
     df.sort_index(ascending=True)
@@ -139,8 +136,6 @@ def portfolio_value_no_rebalance(df, range_down, range_up, initial_qty_0_and_1 =
     qty1 = initial_qty1
 
     end_qty0, end_qty1 = get_lp_asset_qty_after_price_chg(p0, pn, qty0, qty1, range_down, range_up, b_input_pct = True)
-        
-
 
     range_price_up = p0 * (1+range_up)
     range_price_down = p0 * (1+range_down)
@@ -149,7 +144,6 @@ def portfolio_value_no_rebalance(df, range_down, range_up, initial_qty_0_and_1 =
     df[ 'price_range_up'] = range_price_up
     df[ 'price_range_down'] = range_price_down
     df[ 'b_within_range'] =  ps_b_within_range
-
 
     fee_yield =boost_factor* (df['daily_fee_rate']* ps_b_within_range ).sum()
 
@@ -163,9 +157,6 @@ def portfolio_value_no_rebalance(df, range_down, range_up, initial_qty_0_and_1 =
     rst_df = pd.DataFrame(data=[this_mon_array], columns=array_col_names)
 
     return rst_df 
-
-
-
 
 def portfolio_monthly_rebalance(df, range_down, range_up, initial_qty_0_and_1 = None ):
     
@@ -183,14 +174,11 @@ def portfolio_monthly_rebalance(df, range_down, range_up, initial_qty_0_and_1 = 
 
     boost_factor = lib_logic.get_liquidity_boost_given_range(range_down, -0.3 )
 
-
     labels_ym = df['YYYYMM'].unique()
     array_col_names = ['p0', 'end_p', 'qty0', 'qty1', 'end_qty0', 'end_qty1', 'month_fee_yield', 'value_mon_begin', 'value_mon_end']
 
     # Create an empty DataFrame with specified column names
     rst_df_ym = pd.DataFrame(index=labels_ym, columns=array_col_names)
-
-
 
     mon_groups = df.groupby('YYYYMM')
     p0 = starting_price
@@ -209,18 +197,15 @@ def portfolio_monthly_rebalance(df, range_down, range_up, initial_qty_0_and_1 = 
         range_price_down = p0 * (1+range_down)
         ps_b_within_range = df_ym['price'].apply(lambda x: int(x>=range_price_down and x<=range_price_up ))
         
-        
         df.loc[df_ym.index, 'price_range_up'] = range_price_up
         df.loc[df_ym.index, 'price_range_down'] = range_price_down
         df.loc[df_ym.index, 'b_within_range'] =  ps_b_within_range
-        
         
         month_fee_yield =boost_factor* (df_ym['daily_fee_rate']* ps_b_within_range ).sum()
         value_mon_begin = qty0*p0+qty1
         # note the below formula is simplifation. 
         value_mon_end = end_qty0*pn+end_qty1 + (qty0*pn+qty1)*month_fee_yield   #is it so? 
 
-        
         # no need to calculate imp loss for now. we 
         this_mon_array = np.array([p0, pn, qty0, qty1, end_qty0, end_qty1, month_fee_yield, value_mon_begin,value_mon_end ])
         rst_df_ym.loc[ym] = this_mon_array
@@ -238,16 +223,14 @@ def portfolio_monthly_rebalance(df, range_down, range_up, initial_qty_0_and_1 = 
     # Create a dictionary using zip and dictionary comprehension
     rst_dict = {name: value for name, value in zip(rst_names, rst_values)}
 
-
     return rst_dict, rst_df_ym    
 
 
 if __name__ == "__main__":
 
-    b_get_eval_scenarios = True;
+    b_get_eval_scenarios = True
     if b_get_eval_scenarios:
         df_scenarios = get_lp_evaluation_scenarios()
-        print(df_scenarios)
 
     run_rebalance = True
     if run_rebalance:
@@ -256,23 +239,15 @@ if __name__ == "__main__":
         df_price = m.get_df_daily_price(date_begin)
         df_fee = m.get_df_daily_fees(date_begin = date_begin)
         df = m.get_df_comb_price_fee(df_price, df_fee)
-        print("\n check data df first 3 rows:")
-        print(df.head(3))
         
         rst_dict, rst_df_ym = portfolio_monthly_rebalance(df, range_down, range_up )
 
         rst_df_ym.to_clipboard()
-        print("\n results: starting position vs ending position: ")
-        print(rst_dict)
-        print(rst_df_ym)
         
-        
-        
-        print("\n results: no balance: ")
         benchmark_avg_yld_range = -0.3
         range_down = -0.2 # yearly no rebalance
         range_up = -1*range_down/(1+range_down) 
 
         rst_df_ym = portfolio_value_no_rebalance(df, range_down, range_up, benchmark_avg_yld_range = benchmark_avg_yld_range )
             
-        print(rst_df_ym)
+        # print(rst_df_ym)
--- main.py
+++ main.py
@@ -3,29 +3,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import lib_logic as lib_lgc
 import lib_data
-
+import sys
+import inquirer  
+import main_rebal_rebal
+from lib_const import pool_info_list
 
 # print("If you don't have data downloadeded and put as CSV in output folder, please run lib_data.py first!")
 # print("Set load_all_pool_related_data and load_price_related_data = True before run ")
-
-  
-def get_df_daily_fees(date_begin = "2009-01-01", date_end = "3000-01-01"):
+def get_df_daily_fees(pool_address, date_begin = "2009-01-01", date_end = "3000-01-01"):
  
-    pool_address = '0xcbcdf9626bc03e24f779434178a73a0b4bad62ed'
-    df = lib_data.get_uniswap_pool_data_csv(pool_address,date_begin, date_end )
+    df = lib_data.get_uniswap_pool_data_csv(pool_address, date_begin, date_end )
 
     df['daily_fee_rate'] = df['feesUSD'] / df['tvlUSD']
     df['date_i'] = df['date']
     df.set_index('date_i', inplace=True)
     return df[[ 'feesUSD', 'tvlUSD', 'daily_fee_rate']] # 'date',
 
-def get_df_daily_price(date_begin = '2021-03-01', date_end ="3000-01-01"):
+def get_df_daily_price(pool_address, date_begin = '2021-03-01', date_end ="3000-01-01"):
 
     # Load the CSV file
-    df =  lib_data.get_crypto_price_data_csv(date_begin, date_end)
+    df =  lib_data.get_crypto_price_data_csv(pool_address, date_begin, date_end)
+    
+    for pool_info in pool_info_list:
+        if pool_info[0] == pool_address:
+            token0 = pool_info[1]
+            token1 = pool_info[2]
+            break
+            
     # Filter rows related to ETH price in terms of BTC
-    df = df[df['token0_symbol'] == 'WBTC'] 
-    df = df[df['token1_symbol'] == 'WETH']
+    df = df[df['token0_symbol'] == token0] 
+    df = df[df['token1_symbol'] == token1]
     df["YYYYMM"] = df.index.strftime('%Y%m')
     df = add_monthly_price_change(df)
     
@@ -55,7 +62,8 @@ def get_df_comb_price_fee(df_price, df_fee):
     # df.drop(df[ df['month_last_date'].dt.day < 21 ].index, inplace=True) # filter out months with data < 3 weeks
     return df
 
-def get_mon_performance_by_range(range_down, df, benchmark_down = -0.3):
+def get_performance_by_range(range_down, df, rebalance_period='M', benchmark_down=-0.3):
+    if rebalance_period == "M":
         df_mon_chg = df[[ 'YYYYMM', 'Price_chg_vs_MM01']][df['month_last_date'] == df['date']]
         df_mon_chg.dropna(inplace=True)
 
@@ -68,16 +76,19 @@ def get_mon_performance_by_range(range_down, df, benchmark_down = -0.3):
         ret_columns = ['YYYYMM', 'range_down', 'mon_total_price_chg', 'mon_total_fee_yield', 'coverage_rate', 'boost_factor', 'gross_return', 'imp_loss',  'net_return']
 
         boost_factor = lib_lgc.get_liquidity_boost_given_range(prince_range=lower_bound, benchmark=benchmark_lower)
+        
         result_mon = np.empty((len(df_mon_chg), len(ret_columns)))
 
         for mon_i in range(len(df_mon_chg)):
                 yyyymm = df_mon_chg['YYYYMM'].iloc[mon_i]
 
                 mon_total_price_chg = df_mon_chg['Price_chg_vs_MM01'].iloc[mon_i]
-
+                
                 df_yyyymm =  df_mon_day_ret[df_mon_day_ret['YYYYMM'] == yyyymm]
+
                 mon_total_observ = len(df_yyyymm)
                 mon_within_range =df_yyyymm['Price_chg_vs_MM01'].between(lower_bound, upper_bound).sum()
+                
                 coverage_rate= (mon_within_range / mon_total_observ)
 
                 mon_total_fee_yield = df_yyyymm['daily_fee_rate'].sum()
@@ -92,13 +103,99 @@ def get_mon_performance_by_range(range_down, df, benchmark_down = -0.3):
 
         df_mon_result = pd.DataFrame(data=result_mon, columns=ret_columns)
         return df_mon_result
+    # Grouping data by rebalancing period
+    elif rebalance_period == "W": 
+        df['Week'] = df['date'].dt.to_period('W')
+    
+        # Filter for the last day of each week
+        df_week_chg = df[df['date'] == df.groupby('Week')['date'].transform('max')]
+
+        # Extract daily returns excluding the first day of each week
+        df_week_day_ret = df[['Week', 'Price_chg_vs_MM01', 'daily_fee_rate']][df.groupby('Week')['date'].transform('min') != df['date']]
+
+        lower_bound = range_down
+        benchmark_lower = benchmark_down
+        upper_bound = lib_lgc.get_opposite_bin_limit_with_same_liquidity(lower_bound)
+
+        ret_columns = ['Week', 'range_down', 'week_total_price_chg', 'week_total_fee_yield', 'coverage_rate', 'boost_factor', 'gross_return', 'imp_loss', 'net_return']
+
+        boost_factor = lib_lgc.get_liquidity_boost_given_range(prince_range=lower_bound, benchmark=benchmark_lower)
+        
+        result_week = np.empty((len(df_week_chg), len(ret_columns)))
+
+        for week_i in range(len(df_week_chg)):
+            week_period = df_week_chg['Week'].iloc[week_i].start_time.timestamp()  # Convert to Unix timestamp (float)
+
+            week_total_price_chg = df_week_chg['Price_chg_vs_MM01'].iloc[week_i]
+            
+            df_week_period = df_week_day_ret[df_week_day_ret['Week'] == df_week_chg['Week'].iloc[week_i]]
+            
+            week_total_observ = len(df_week_period)
+            week_within_range = df_week_period['Price_chg_vs_MM01'].between(lower_bound, upper_bound).sum()
+            
+            coverage_rate = week_within_range / week_total_observ
+            week_total_fee_yield = df_week_period['daily_fee_rate'].sum()
+            gross_return = week_total_fee_yield * coverage_rate * boost_factor
+            imp_loss = lib_lgc.get_impermanent_loss_given_range(week_total_price_chg, lower_bound)
+            net_return = (1 + gross_return) * (1 + imp_loss) - 1
+
+            result_week[week_i, :] = np.array([week_period, lower_bound, week_total_price_chg, week_total_fee_yield, coverage_rate, boost_factor, gross_return, imp_loss, net_return])
+
+        df_week_result = pd.DataFrame(data=result_week, columns=ret_columns)
+        return df_week_result
+
+    else :    
+        df_daily_chg = df[['date', 'Price_chg_vs_MM01']][df['date'] == df['date']]
+        df_daily_chg.dropna(inplace=True)
+
+        # Daily return calculations
+        df_daily_ret = df[['date', 'Price_chg_vs_MM01', 'daily_fee_rate']][df['date'] == df['date']]
+
+        lower_bound = range_down
+        benchmark_lower = benchmark_down
+        upper_bound = lib_lgc.get_opposite_bin_limit_with_same_liquidity(lower_bound)
+
+        ret_columns = ['date', 'range_down', 'daily_total_price_chg', 'daily_total_fee_yield', 
+                    'coverage_rate', 'boost_factor', 'gross_return', 'imp_loss', 'net_return']
+
+        # Get liquidity boost factor
+        boost_factor = lib_lgc.get_liquidity_boost_given_range(prince_range=lower_bound, benchmark=benchmark_lower)
+
+        result_daily = np.empty((len(df_daily_chg), len(ret_columns)), dtype=object)  # Change to object dtype
+
+        for daily_i in range(len(df_daily_chg)):
+            date = df_daily_chg['date'].iloc[daily_i]
+
+            daily_total_price_chg = df_daily_chg['Price_chg_vs_MM01'].iloc[daily_i]
+            df_date = df_daily_ret[df_daily_ret['date'] == date]
+
+            if df_date.empty:
+                print("No data for the date:", date)
+                continue
+
+            daily_total_observ = len(df_date)
+            daily_within_range = df_date['Price_chg_vs_MM01'].between(lower_bound, upper_bound).sum()
+            coverage_rate = (daily_within_range / daily_total_observ) if daily_total_observ > 0 else 0
+            daily_total_fee_yield = df_date['daily_fee_rate'].sum()
+
+            gross_return = daily_total_fee_yield * coverage_rate * boost_factor
+            imp_loss = lib_lgc.get_impermanent_loss_given_range(daily_total_price_chg, lower_bound)
+            net_return = (1 + gross_return) * (1 + imp_loss) - 1
+
+            result_daily[daily_i, :] = np.array([
+                date, lower_bound, daily_total_price_chg, daily_total_fee_yield,
+                coverage_rate, boost_factor, gross_return, imp_loss, net_return
+            ])
+
+        df_daily_result = pd.DataFrame(data=result_daily, columns=ret_columns)
+        return df_daily_result
 
 
-def get_full_range_performance(range_down, df, benchmark_range =-0.3):
-    array_range_rst =  np.empty((len(range_down),4))
+def get_full_range_performance(range_down, df, rebalance_period = "M", benchmark_range =-0.3):
+    array_range_rst = np.empty((len(range_down),4))
     for range_i in range(len(range_down)):
             range_down_i = range_down[range_i]
-            df_i = get_mon_performance_by_range(range_down_i, df, benchmark_down=benchmark_range)
+            df_i = get_performance_by_range(range_down_i, df, rebalance_period, benchmark_down=benchmark_range)
             average_gross_return = df_i['gross_return'].median() #.mean()
             average_imp_loss = df_i['imp_loss'].median() #mean()
             average_net_return = df_i['net_return'].median() #.mean()
@@ -146,15 +243,38 @@ def main ():
     range_down = np.arange(-0.5, 0, 0.02)
     benchmark_range = -0.3
 
+    questions = [
+        inquirer.List('option',
+            message="Choose one",
+            choices=['USDC/WETH' , 'WBTC/WETH'],
+        ),
+    ]
+    answers = inquirer.prompt(questions)
+    pool = main_rebal_rebal.pool_address(answers['option'])
+    
+    questions = [
+        inquirer.List('option',
+            message="Choose Rebalancing Frequency(days)",
+            choices=["Day", "Week", "Month"],
+        )
+    ]
 
-    df_price = get_df_daily_price(date_begin,date_end=date_end)
+    answers = inquirer.prompt(questions)
+    rebalance_period = answers['option']
+    
+    df_price = get_df_daily_price(pool, date_begin,date_end=date_end)
     print(df_price.head())
-    df_fee = get_df_daily_fees(date_begin = date_begin, date_end=date_end)
+    df_fee = get_df_daily_fees(pool, date_begin = date_begin, date_end=date_end)
     df = get_df_comb_price_fee(df_price, df_fee)
     print("\n check df")
     print(df.head())
-    df_result = get_full_range_performance(range_down, df, benchmark_range=benchmark_range)
-
+    if rebalance_period == "Day":
+        rebalance_period = "D"
+    elif rebalance_period == "Week":
+        rebalance_period = "W"
+    elif rebalance_period == "Month":
+        rebalance_period = "M"
+    df_result = get_full_range_performance(range_down, df, rebalance_period, benchmark_range)
     result_file_name = 'output/eth_btc_lp_range_result_v3.csv' 
     df_result.to_csv(result_file_name, index=False)
     print("result saved to ", result_file_name)
